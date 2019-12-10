0cde8ebf7dafea12144c40892674c9a709efc343
unknown <gablorian@gmail.com>
2019-12-10 22:29:21 +0100

    minor changes

--- my-app/src/About.js
+++ my-app/src/About.js
@@ -9,11 +9,11 @@ function About() {
                 <h2 className='textforinvi'> O nas</h2>
             </div>
             <div className='aboutDiv'>
-                <div>
+
                     <div className='textDiv'>
                         <p>Witaj na stronie naszej firmy! Zajmujemy się sprzedażą motocykli już od 13 lat. W naszej ofercie znajduje się około 100 motocykli, uznanych marek ze szczególnym naciskiem na BMW. Nasze motocykle pochodzą ze sprawdzonych źródeł we Włoszech.</p>
                     </div>
-                </div>
+
                 <div className='photoDivOne'>
 
                 </div>
@@ -25,21 +25,20 @@ function About() {
                 <div className='photoDivTwo'>
 
                 </div>
-                <div>
+
                     <div className='textDiv'>
                         <p>Oferujemy również możliwość transportu kupionego u nas motocykla. Przewieziemy go na zamykanej pace busa, zabezpieczonego pasami oraz piankami, by zapewnić bezpieczeństwo. Koszt takiego przewozu to około 1 zł za kilometr, liczone w jedną stronę.</p>
                     </div>
-                </div>
+
             </div>
             <div className='spacer'>
 
             </div>
             <div className='aboutDiv'>
-                <div>
                     <div className='textDiv'>
                         <p>Wszystkie nasze motocykle objętę są gwarancją rozruchową na okres 1 miesiąca lub 1000 kilometrów po zakupie (szczegóły u sprzedawcy).</p>
                     </div>
-                </div>
+
                 <div className='photoDivThree'>
 
                 </div>
@@ -51,11 +50,11 @@ function About() {
                 <div className='photoDivFour'>
 
                 </div>
-                <div>
+
                     <div className='textDiv'>
                         <p>Przyjmujemy motocykle w rozliczeniu! Nie podajemy wycen przez telefon! Żebyśmy mogli wycenić motocykl najlepiej nim przyjechać po wcześniejszym kontakcie lub wysłać nam na adres mailowy krótki opis, proponowaną cenę za motocykl, oraz dokładne zdjęcia.</p>
                     </div>
-                </div>
+
             </div>
             <div className='spacer'>
 
